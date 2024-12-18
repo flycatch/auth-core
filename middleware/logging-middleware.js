29a793afae231a85c4f9faaa106a00e51573fe76
ansad.k <ansad.k@flycatchtech.com>
2024-12-18 14:21:27 +0530

    refactor: removed unwanted files

--- middleware/logging-middleware.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../libs/wintson-logger');
-
- const loggingMiddleware = (req, res, next) => {
-    logger.info(`${req.method} - ${req.url}`);
-    next();
-}
-
-module.exports = loggingMiddleware;
