bad2840ef2e5f94c15ca198bc7dc513fa82943d9
ansad.k <ansad.k@flycatchtech.com>
2025-01-28 10:11:36 +0530

    feat: refreshtoken added

--- src/routes/setup-google-oath.js
+++ src/routes/setup-google-oath.js
@@ -5,11 +5,37 @@ const express = require("express");
 const logger = require("../lib/wintson.logger");
 
 module.exports = (router, config) => {
-  router.use(express.json()); 
+  router.use(express.json());
   router.get(
     "/auth/google/login",
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
+
+  const createAccessToken = async (user) => {
+    const payload = {
+      id: user.id,
+      username: user.username,
+      type: "access",
+    };
+
+    const accessToken = jwt.sign(payload, config.jwt.secret, {
+      expiresIn: config.jwt.jwt_expires || "8h",
+    });
+    return accessToken;
+  };
+
+  const createRefreshToken = async (user) => {
+    const payload = {
+      id: user.id,
+      username: user.username,
+      type: "refresh",
+    };
+
+    const refreshToken = jwt.sign(payload, config.jwt.secret, {
+      expiresIn: "7d",
+    });
+    return refreshToken;
+  };
 
   router.get(
     "/auth/google/callback",
@@ -17,20 +43,25 @@ module.exports = (router, config) => {
     async (req, res) => {
       try {
         logger.info("Handling Google OAuth callback");
-        // Create a JWT token after successful login
-        const payload = {
-          id: req.user.id,
-          username: req.user.username,
-          // email: req.user.email,
-        };
-        const token = jwt.sign(payload, config.google.secret, {
-          expiresIn: "8h",
-        });
+        // // Create a JWT token after successful login
+        // const payload = {
+        //   id: req.user.id,
+        //   username: req.user.username,
+        //   type: "access"
+        //   // email: req.user.email,
+        // };
+        // const token = jwt.sign(payload, config.google.secret, {
+        //   expiresIn: "8h",
+        // });
+
+        const accessToken = await createAccessToken(req.user);
+        const refreshToken = await createRefreshToken(req.user);
 
         // Send the token in the response
         res.json({
           message: "Google OAuth successful",
-          token,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
         });
         logger.info("User successfully logged in with Google OAuth");
       } catch (err) {
